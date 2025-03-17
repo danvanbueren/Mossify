a87c689b505da8f65e58f5971b2f22b3fb31537d
Dan Van Bueren <danvb10@gmail.com>
2025-03-17 15:25:37 +0200

    Add logger

--- app/src/main/java/me/danvb10/mossify/MossifyClient.java
+++ app/src/main/java/me/danvb10/mossify/MossifyClient.java
@@ -2,9 +2,12 @@ package me.danvb10.mossify;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import static me.danvb10.mossify.Mossify.LOGGER;
+
 public class MossifyClient implements ClientModInitializer {
+
     @Override
     public void onInitializeClient() {
-
+        LOGGER.info("onInitializeClient");
     }
 }
