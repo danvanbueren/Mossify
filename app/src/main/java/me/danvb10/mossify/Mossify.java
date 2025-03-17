6473694f42d352e7f632c71286c85b65dc904889
Dan Van Bueren <danvb10@gmail.com>
2025-03-17 15:25:46 +0200

    Comment

--- app/src/main/java/me/danvb10/mossify/Mossify.java
+++ app/src/main/java/me/danvb10/mossify/Mossify.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mossify implements ModInitializer {
+
+	// Create logger
 	public static final String MOD_ID = "mossify";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
