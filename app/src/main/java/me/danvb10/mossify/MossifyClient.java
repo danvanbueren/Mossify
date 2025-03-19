b08c0545e2d5b2781088978f79ef1e159bdbed3f
Dan Van Bueren <danvb10@gmail.com>
2025-03-19 02:18:34 +0200

    Basic block rendering

--- app/src/main/java/me/danvb10/mossify/MossifyClient.java
+++ app/src/main/java/me/danvb10/mossify/MossifyClient.java
@@ -3,17 +3,78 @@ package me.danvb10.mossify;
 import me.danvb10.mossify.render.MossRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.math.BlockPos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static me.danvb10.mossify.Mossify.LOGGER;
+import java.util.Set;
 
 public class MossifyClient implements ClientModInitializer {
+
+    // Mod ID
+    public static final String MOD_ID = "mossify";
+
+    // Logger
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    // Minecraft Client
+    public static MinecraftClient CLIENT;
+
+    // Player position
+    public static BlockPos PLAYER_POSITION;
+
+
+    public static final Set<Block> ALLOWED_TEXTURES = Set.of(
+            Blocks.GRASS_BLOCK,
+            Blocks.DIRT,
+            Blocks.STONE,
+            Blocks.GRAVEL,
+            Blocks.GRANITE,
+            Blocks.ANDESITE,
+            Blocks.DIORITE,
+            Blocks.SAND,
+            Blocks.SANDSTONE,
+            Blocks.RED_SANDSTONE,
+            Blocks.TUFF,
+            Blocks.DEEPSLATE,
+            Blocks.COAL_ORE,
+            Blocks.COPPER_ORE,
+            Blocks.DEEPSLATE_COPPER_ORE,
+            Blocks.DEEPSLATE_COAL_ORE,
+            Blocks.DEEPSLATE_DIAMOND_ORE,
+            Blocks.SNOW_BLOCK,
+            Blocks.POWDER_SNOW,
+            Blocks.ICE,
+            Blocks.BLUE_ICE,
+            Blocks.FROSTED_ICE,
+            Blocks.PACKED_ICE
+
+    );
+
+    public static final Set<Block> ALLOWED_ENVIRONMENTS = Set.of(
+            Blocks.AIR,
+            Blocks.CAVE_AIR,
+            Blocks.VOID_AIR,
+            Blocks.SHORT_GRASS,
+            Blocks.TALL_GRASS,
+            Blocks.CORNFLOWER,
+            Blocks.WATER,
+            Blocks.LAVA,
+            Blocks.SNOW,
+            Blocks.GLOW_LICHEN
+    );
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("onInitializeClient");
 
-        // Register the MossRenderer to run after entity rendering
-        WorldRenderEvents.BEFORE_DEBUG_RENDER.register(MossRenderer::render);
+        CLIENT = MinecraftClient.getInstance();
+
+        // Register the MossRenderer to run
+        WorldRenderEvents.AFTER_ENTITIES.register(MossRenderer::render);
 
     }
 }
--- app/src/main/java/me/danvb10/mossify/render/MossRenderer.java
+++ app/src/main/java/me/danvb10/mossify/render/MossRenderer.java
@@ -1,15 +1,22 @@
 package me.danvb10.mossify.render;
 
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import org.joml.Quaternionf;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
+
+import static me.danvb10.mossify.MossifyClient.*;
 
 public class MossRenderer {
 
@@ -19,28 +26,64 @@ public class MossRenderer {
         if (client.player == null) return;
         if (client.world == null) return;
 
-        BlockPos cameraPosition = client.player.getBlockPos();
-
-        float size = 0.25f;
+        PLAYER_POSITION = client.player.getBlockPos();
 
 
-        //drawCube(context, new BlockPos(0, 100, 0), 0.15f);
-        //drawCube(context, new BlockPos(5, 100, 0), 0.25f);
-        drawCube(context, new BlockPos(10, 100, 0), 0.50f);
+        for (int x = -10; x <= 10; x++) {
+            for (int y = -3; y <= 3; y++) {
+                for (int z = -10; z <= 10; z++) {
+                    BlockPos thisBlockPosition = PLAYER_POSITION.add(x, y, z);
+                    BlockPos underBlockPosition = PLAYER_POSITION.add(x, y - 1, z);
 
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
-                for (int z = -1; z <= 1; z++) {
-                    //BlockPos position = cameraPosition.add(x, y+2, z-4);
-                    //BlockPos position = new BlockPos(0, 100, 0);
-                    //BlockState state = client.world.getBlockState(position);
-                    //drawCube(context, position, size);
+                    BlockState thisBlockState = client.world.getBlockState(thisBlockPosition);
+                    BlockState underBlockState = client.world.getBlockState(underBlockPosition);
+
+                    Block thisBlock = thisBlockState.getBlock();
+                    Block underBlock = underBlockState.getBlock();
+
+                    if (ALLOWED_TEXTURES.contains(underBlock)) {
+                        if (ALLOWED_ENVIRONMENTS.contains(thisBlock)) {
+                            int r = 255;
+                            int g = 255;
+                            int b = 255;
+                            int a = 255;
+
+                            Sprite sprite = client.getBlockRenderManager().getModel(underBlockState).getParticleSprite();
+
+                            if (underBlock == Blocks.GRASS_BLOCK) {
+                                // Get top of grass sprite
+                                sprite = client.getBlockRenderManager()
+                                        .getModel(underBlockState)
+                                        .getQuads(underBlockState, net.minecraft.util.math.Direction.UP, client.world.random)
+                                        .getFirst()
+                                        .getSprite();
+
+                                // Get biome color for grass
+                                int color = MinecraftClient.getInstance().getBlockColors().getColor(underBlockState, client.world, underBlockPosition, 0);
+
+                                // Extract RGB values from color integer
+                                r = (color >> 16) & 0xFF;
+                                g = (color >> 8) & 0xFF;
+                                b = color & 0xFF;
+                            }
+
+
+                            float size = 8f;
+
+                            int light = WorldRenderer.getLightmapCoordinates(client.world, thisBlockPosition);
+
+                            drawCube(context, thisBlockPosition, size, sprite, light, r, g, b, a);
+                        }
+                    }
+
                 }
             }
         }
     }
 
-    public static void drawCube(WorldRenderContext context, BlockPos position, float size) {
+    public static void drawCube(WorldRenderContext context, BlockPos position, float size, Sprite sprite, int light, int r, int g, int b, int a) {
+
+        size /= 32.0f;
 
         // Start
         MatrixStack matrixStack = context.matrixStack();
@@ -49,58 +92,34 @@ public class MossRenderer {
 
         // Move to world space
         matrixStack.translate(
-                position.getX() - context.camera().getPos().x,
-                position.getY() - context.camera().getPos().y,
-                position.getZ() - context.camera().getPos().z
+                position.getX() - context.camera().getPos().x + size,
+                position.getY() - context.camera().getPos().y + size,
+                position.getZ() - context.camera().getPos().z + size
         );
-
-        // Get matrix
-        Matrix4f matrix = matrixStack.peek().getPositionMatrix();
 
         // Get VertexConsumerProvider
         VertexConsumerProvider consumers = context.consumers();
         if (consumers == null) return;
 
-        // Render textures
-        VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getSolid());
-        //Identifier stoneTexture = Identifier.tryParse("textures/blocks/stone_stone.png");
-        //VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getEntitySolid(stoneTexture));
+        // Define UV texture coords dynamically
+        float uMin = sprite.getMinU(), vMin = sprite.getMinV(), uMax = sprite.getMaxU(), vMax = sprite.getMaxV();
 
-        // Define UV texture coordinates for stone -- TODO: Remove hardcode and add dynamic detection
-        float uMin = 0f, vMin = 0f, uMax = 1f, vMax = 1f;
+        // Render textures
+        VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getCutout());
 
         /* *********** Draw quads to create cube *********** */
-
-        // South-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf(), new Vector3f(0, 0, 1));
-
-        // North-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf().rotateY((float) Math.toRadians(180)), new Vector3f(0, 0, -1));
-
-        // East-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf().rotateY((float) Math.toRadians(90)), new Vector3f(1, 0, 0));
-
-        // West-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf().rotateY((float) Math.toRadians(-90)), new Vector3f(-1, 0, 0));
-
-        // Down-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf().rotateX((float) Math.toRadians(90)), new Vector3f(0, -1, 0));
-
-        // Up-facing
-        drawQuad(matrixStack, vertexConsumer, matrix, size, uMin, vMin, uMax, vMax,
-                new Quaternionf().rotateX((float) Math.toRadians(-90)), new Vector3f(0, 1, 0));
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf(), new Vector3f(0, 0, 1), light, r, g, b, a); // South-facing
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf().rotateY((float) Math.toRadians(180)), new Vector3f(0, 0, -1), light, r, g, b, a); // North-facing
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf().rotateY((float) Math.toRadians(90)), new Vector3f(1, 0, 0), light, r, g, b, a); // East-facing
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf().rotateY((float) Math.toRadians(-90)), new Vector3f(-1, 0, 0), light, r, g, b, a); // West-facing
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf().rotateX((float) Math.toRadians(90)), new Vector3f(0, -1, 0), light, r, g, b, a); // Down-facing
+        drawQuad(matrixStack, vertexConsumer, size, uMin, vMin, uMax, vMax, new Quaternionf().rotateX((float) Math.toRadians(-90)), new Vector3f(0, 1, 0), light, r, g, b, a); // Up-facing
 
         // End
         matrixStack.pop();
     }
 
-    private static void drawQuad(MatrixStack matrixStack, VertexConsumer vertexConsumer, Matrix4f matrix, float size,
-                                 float uMin, float vMin, float uMax, float vMax, Quaternionf rotation, Vector3f translation) {
+    private static void drawQuad(MatrixStack matrixStack, VertexConsumer vertexConsumer, float size, float uMin, float vMin, float uMax, float vMax, Quaternionf rotation, Vector3f translation, int light, int r, int g, int b, int a) {
         // Start
         matrixStack.push();
 
@@ -114,10 +133,10 @@ public class MossRenderer {
         Matrix4f transformedMatrix = matrixStack.peek().getPositionMatrix();
 
         // Draw quad
-        vertexConsumer.vertex(transformedMatrix, -size, -size, 0).texture(uMin, vMin).color(255, 255, 255, 255).light(0xF000F0).normal(0, 0, 1);
-        vertexConsumer.vertex(transformedMatrix, size, -size, 0).texture(uMax, vMin).color(255, 255, 255, 255).light(0xF000F0).normal(0, 0, 1);
-        vertexConsumer.vertex(transformedMatrix, size, size, 0).texture(uMax, vMax).color(255, 255, 255, 255).light(0xF000F0).normal(0, 0, 1);
-        vertexConsumer.vertex(transformedMatrix, -size, size, 0).texture(uMin, vMax).color(255, 255, 255, 255).light(0xF000F0).normal(0, 0, 1);
+        vertexConsumer.vertex(transformedMatrix, -size, -size, 0).texture(uMin, vMin).color(r, g, b, a).light(light).normal(0, 0, 1);
+        vertexConsumer.vertex(transformedMatrix, size, -size, 0).texture(uMax, vMin).color(r, g, b, a).light(light).normal(0, 0, 1);
+        vertexConsumer.vertex(transformedMatrix, size, size, 0).texture(uMax, vMax).color(r, g, b, a).light(light).normal(0, 0, 1);
+        vertexConsumer.vertex(transformedMatrix, -size, size, 0).texture(uMin, vMax).color(r, g, b, a).light(light).normal(0, 0, 1);
 
         // Stop
         matrixStack.pop();
